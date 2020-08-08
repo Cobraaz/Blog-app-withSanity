f3961bbbd7f081631fa575f0eb21e258ce9ea523
Cobraaz <anujbansal077@gmail.com>
2020-08-08 19:50:46 +0530

    setup of nextjs project

---
+++ components/AuthorIntro.js
@@ -0,0 +1,29 @@
+import { Row, Col, Media, Image } from "react-bootstrap";
+
+const AuthorIntro = () => (
+  <Row>
+    <Col md="8">
+      {/* AUTHOR INTRO STARTS */}
+      <Media className="mb-4 admin-intro">
+        <Image
+          roundedCircle
+          width={64}
+          height={64}
+          className="mr-3"
+          src="https://www.gravatar.com/avatar/b0a2614280cf802d8bb4c26bc6b7dcc4?s=200&r=pg&d=mm"
+          alt="Generic placeholder"
+        />
+        <Media.Body>
+          <h5 className="font-weight-bold mb-0">Hello Friends,</h5>
+          <p className="welcome-text">
+            My name is Anuj Bansal and I am an experienced software engineer and
+            freelance developer. and this is my blog page.
+          </p>
+        </Media.Body>
+      </Media>
+      {/* AUTHOR INTRO ENDS */}
+    </Col>
+  </Row>
+);
+
+export default AuthorIntro;

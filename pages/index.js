fda104880cf3a20373671012a8074a6e2b079d39
Cobraaz <anujbansal077@gmail.com>
2020-08-09 13:47:48 +0530

    starting with sanity

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,42 @@ import AuthorIntro from "components/AuthorIntro";
 import CardItem from "components/CardItem";
 import CardListItem from "components/CardListItem";
 
-export default function Home() {
+import { getAllBlogs } from "lib/api";
+
+export default function Home({ blogs }) {
   return (
     <PageLayout>
       <AuthorIntro />
       <hr />
       <Row className="mb-5">
-        <Col md="10">
+        {/* <Col md="10">
           <CardListItem />
-        </Col>
-        <Col md="4">
-          <CardItem />
-        </Col>
+        </Col> */}
+        {blogs.map((blog) => (
+          <Col key={blog.slug} md="4">
+            <CardItem
+              author={blog.author}
+              title={blog.title}
+              subtitle={blog.subtitle}
+              date={blog.date}
+              image={blog.coverImage}
+              link={{
+                href: "/blogs/[slug]",
+                as: `/blogs/${blog.slug}`,
+              }}
+            />
+          </Col>
+        ))}
       </Row>
     </PageLayout>
   );
+}
+
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs,
+    },
+  };
 }
